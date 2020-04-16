a80c3116eea7cd4d26fac6e2d2138aff45d04581
faruqneo <faruq_khan@yahoo.com>
2020-04-16 12:24:01 +0530

    set db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-async function bootstrap() {
+const PORT = process.env.PORT || 8000;
+
+(async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
-bootstrap();
+  app.enableCors();
+
+  const options = new DocumentBuilder()
+  .setTitle('AFkart')
+  .setDescription('API documention of afkart store. ')
+  .setVersion('1.0')
+  .build()
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(PORT);
+})()
+
