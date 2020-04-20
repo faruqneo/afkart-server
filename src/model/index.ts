758287b4a0e0d46622ce8a08d3194994b7e25b01
faruqneo <faruq_khan@yahoo.com>
2020-04-20 10:27:11 +0530

    working login

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-
-
-export const Models = [];
-=======
 import * as mongoose from 'mongoose';
 
 import { UserSchema } from "./users.schema";
@@ -11,4 +6,3 @@ import { UserSchema } from "./users.schema";
 export const Models = [
     { name: 'User', schema: new mongoose.Schema(UserSchema) }
 ];
->>>>>>> fb59fe3cbe8de59dd20ed236fca5bb0f8ae35cd4
